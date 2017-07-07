dbcef65762190fdc2afc546b76602b9f71103e14
Menq <sahak@ukdevplus.co.uk>
2017-07-07 22:57:54 +0400

    start

---
+++ src/app/Providers/AvatarServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Avatar\Avatar;
+
+//use TorMorten\Eventy;
+
+use App\helpers\EmailScHelper;
+use App\helpers\Tabs;
+use App\Modules\Users\User;
+use Blade;
+use File;
+use Illuminate\Support\ServiceProvider;
+use View;
+
+
+class AvatarServiceProvider extends ServiceProvider
+{
+
+
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
